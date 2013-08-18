375825cfa3520b90e1726b2055170d24c8658c27
Fulvio <fcusumano@gmail.com>
2013-08-18 21:54:53 +1000

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
  * http://www.getpebble.com/
  *
  * Copyright (C) 2013 Fulvio Cusumano
+ * http://www.gotnull.com/
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
